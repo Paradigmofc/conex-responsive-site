f75cd656242166ae7e1977f4ed23fdcc95650c37
Alexey <paradigmofc@gmail.com>
2021-12-15 23:49:03 +0200

    php fix

--- mail.php
+++ mail.php
@@ -8,33 +8,27 @@ $name = $_POST['user_name'];
 $text = $_POST['user_text'];
 $email = $_POST['user_email'];
 
-$mail->SMTPDebug = 3;                             // Enable verbose debug output
+// $mail->SMTPDebug = 3;                             // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'ladnosmazan2003@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'javascripthyeta)'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'lexa.animewnik@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'wexqtubpuptrvrkb'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('ladnosmazan2003@gmail.com');  // от кого будет уходить письмо?
-$mail->addAddress('paradigmofc@gmail.com');  // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->setFrom('lexa.animewnik@gmail.com');  // от кого будет уходить письмо?
+$mail->addAddress('lexa.animewnik@gmail.com');  // Кому будет уходить письмо 
+$mail->isHTML(true);                          
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Subject = 'Заявка с сайта Conex Crypto';
+$mail->Body    = '' .$name . ' оставил заявку,<br> его сообщение:'.$text .' <br>Почта этого пользователя: ' .$email;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
     echo 'Error';
 } else {
-    header('location: thank-you.html');
+    header('location: index.html');
 }
 ?>
